de4097756f9888bb91cae124bd7794c7fe119921
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-06-27 11:24:00 +0600

    Time: 0 ms (100.00%) | Memory: 11.1 MB (95.89%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -2,29 +2,44 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
-        vector<int> b(n);
-        for(int x = 0; x < n; x++){
-            int curi = 0;
-            for(int i = x; i < n; i++)
-            {
-                b[curi++] = nums[i];
-            }
-            for(int i = 0; i < x; i++)
-            {
-                b[curi++] = nums[i];
-            }
-            bool sorted = true;
-            for(int i = 0; i < n-1; i++)
-            {
-                if(b[i]>b[i+1])
-                {
-                    sorted = false;
-                    break;
-                }
-            }
-            if(sorted)
-                return true;
+
+        // NaiveApprach
+        // vector<int> b(n);
+        // for(int x = 0; x < n; x++){
+        //     int curi = 0;
+        //     for(int i = x; i < n; i++)
+        //     {
+        //         b[curi++] = nums[i];
+        //     }
+        //     for(int i = 0; i < x; i++)
+        //     {
+        //         b[curi++] = nums[i];
+        //     }
+        //     bool sorted = true;
+        //     for(int i = 0; i < n-1; i++)
+        //     {
+        //         if(b[i]>b[i+1])
+        //         {
+        //             sorted = false;
+        //             break;
+        //         }
+        //     }
+        //     if(sorted)
+        //         return true;
+        // }
+        // return false;
+
+        //MyApproach
+        int c = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if(nums[i]<nums[i-1])
+                c++;
+            if(c>1)
+                return false;
         }
-        return false;
+        if(nums[0]<nums[n-1])
+            c++;
+        return c < 2;
     }
 };
