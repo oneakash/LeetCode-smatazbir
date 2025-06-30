c0708ed9077982f4dc801b91f0417509fd89c1e1
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-06-30 16:29:58 +0600

    Time: 0 ms (100.00%) | Memory: 20.7 MB (58.26%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i = 1; i < nums.size(); i+=2)
-        {
-            if(nums[i]!=nums[i-1])
-                return nums[i-1];
-        }
-        return nums[nums.size()-1];
+
+        //Time -> O(nlogn)
+        // sort(nums.begin(), nums.end());
+        // for(int i = 1; i < nums.size(); i+=2)
+        // {
+        //     if(nums[i]!=nums[i-1])
+        //         return nums[i-1];
+        // }
+        // return nums[nums.size()-1];
+
+        int a = 0;
+        for(auto &i : nums)
+            a ^= i;
+        return a;
+        //Complete in O(n) time
     }
 };
