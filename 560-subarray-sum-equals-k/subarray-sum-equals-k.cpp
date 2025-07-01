d8ecbd5446bc90b231f864305470e7f74b7c257b
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-07-01 15:02:08 +0600

    Time: 37 ms (83.02%) | Memory: 45.4 MB (69.81%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,11 +3,12 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> mp;
         int r = 0, psum = 0;
+        mp[0]=1;
         for(int i = 0; i < nums.size(); i++)
         {
             psum += nums[i];
-            if(psum == k)
-                r++;
+            // if(psum == k)
+            //     r++;
             if(mp.find(psum - k)!=mp.end())
                 r+=mp[psum-k];
             mp[psum]++;
