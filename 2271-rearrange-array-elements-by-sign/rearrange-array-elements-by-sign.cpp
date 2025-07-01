05d57c50ce27a47d000477b2cd0c5531bf7bfccc
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-07-01 23:04:43 +0600

    Time: 9 ms (40.38%) | Memory: 128.2 MB (94.83%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-
-        //Time -> O(n), Space -> O(n)
-        // vector<int> ve, vo, v;
-        // for(int i = 0; i < nums.size(); i++)
-        // {
-        //     if(nums[i]<0)
-        //         vo.push_back(nums[i]);
-        //     else
-        //         ve.push_back(nums[i]);
-        // }
-        // for(int i= 0; i < nums.size()/2; i++)
-        // {
-        //     v.push_back(ve[i]);
-        //     v.push_back(vo[i]);
-        // }
-        // return v;
-
-        vector<int> res(nums.size(),0);
-        int p = 0, n = 1;
-        for(int i = 0; i<nums.size(); i++)
+        int n = nums.size();
+        vector<int> res(n,0);
+        int p = 0, neg = 1;
+        for(int i = 0; i<n; i++)
         {
             if(nums[i]<0)
             {
-                res[n] = nums[i];
-                n+=2;
+                res[neg] = nums[i];
+                neg+=2;
             }
             else
             {
