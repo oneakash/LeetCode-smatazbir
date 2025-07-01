b6fc97dc3a53e27e1c8d8f6e61ea0fa769608e23
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-07-01 22:50:40 +0600

    Time: 9 ms (40.38%) | Memory: 128.3 MB (79.43%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> ve, vo, v;
-        for(int i = 0; i < nums.size(); i++)
+
+        //Time -> O(n), Space -> O(n)
+        // vector<int> ve, vo, v;
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //     if(nums[i]<0)
+        //         vo.push_back(nums[i]);
+        //     else
+        //         ve.push_back(nums[i]);
+        // }
+        // for(int i= 0; i < nums.size()/2; i++)
+        // {
+        //     v.push_back(ve[i]);
+        //     v.push_back(vo[i]);
+        // }
+        // return v;
+
+        vector<int> res(nums.size(),0);
+        int p = 0, n = 1;
+        for(int i = 0; i<nums.size(); i++)
         {
             if(nums[i]<0)
-                vo.push_back(nums[i]);
+            {
+                res[n] = nums[i];
+                n+=2;
+            }
             else
-                ve.push_back(nums[i]);
+            {
+                res[p] = nums[i];
+                p+=2;
+            }
         }
-        for(int i= 0; i < nums.size()/2; i++)
-        {
-            v.push_back(ve[i]);
-            v.push_back(vo[i]);
-        }
-        return v;
+        return res;
     }
 };
