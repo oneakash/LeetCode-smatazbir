2d5319e38f50115cfeb626082a7ff040ef75332f
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-07-02 12:34:53 +0600

    Time: 79 ms (70.32%) | Memory: 88.7 MB (74.06%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,18 +4,21 @@ public:
         int n = nums.size(), le = INT_MIN, c =0, a = 1;
         if(n<1)
             return 0;
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < n; i++){
-            if(le == nums[i]-1){
-                c++;
-                
-                le = nums[i];
+        unordered_set<int> st;
+        for(auto &i : nums)
+            st.insert(i);
+        for(auto &it : st)
+        {
+            if(st.find(it-1)==st.end())
+            {
+                int x = it;
+                int c=1;
+                while(st.find(++x)!=st.end())
+                {
+                    c++;
+                }
+                a = max(a, c);
             }
-            else if(le != nums[i]){
-                c=1;
-                le = nums[i];
-            }
-            a = max(a,c);
         }
         return a;
     }
