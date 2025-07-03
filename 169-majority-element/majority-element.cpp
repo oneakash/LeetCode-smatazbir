13593fff83f809f85d1cb511111f0a1ad6762531
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-07-03 12:58:13 +0600

    Time: 1 ms (36.79%) | Memory: 28 MB (85.34%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
-        //Time -> O(n), Space -> O(n)
-        // unordered_map<int, int> mp;
-        // for(int i = 0; i < nums.size(); i++)
-        // {
-        //     mp[nums[i]]++;
-        // }
-        // for(int i=0; i < nums.size(); i++)
-        // {
-        //     if(mp[nums[i]]>(nums.size()/2))
-        //         return nums[i];
-        // }
-        // return 0;
-
-        //Time -> O(n), Space -> O(1);
-        //Moor Voting  Algorithm
         int count = 0;
         int candidate = 0;
         for(int i : nums)
