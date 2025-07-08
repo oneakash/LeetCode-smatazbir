995e89688a34afaaf54d7b83398569a1bcced747
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-07-08 19:42:21 +0600

    Time: 0 ms (100.00%) | Memory: 29.5 MB (79.25%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -8,12 +8,19 @@ public:
         //     nums.pop_back();
         // }
 
-        int n = nums.size();
-        vector<int> b(n);
-        for(int idx = 0; idx < n; idx++)
-        {
-            b[(idx+k)%n] = nums[idx];
-        }
-        nums = b;
+        // Space O(n)
+        // int n = nums.size();
+        // vector<int> b(n);
+        // for(int idx = 0; idx < n; idx++)
+        // {
+        //     b[(idx+k)%n] = nums[idx];
+        // }
+        // nums = b;
+
+        int n=nums.size();
+        k = k % n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
