adc4b723e570370f3d6c33e11d89e1864fd5c3b0
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-07-08 21:23:48 +0600

    Time: 0 ms (100.00%) | Memory: 15.2 MB (32.99%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -8,14 +8,14 @@ public:
                 return mid;
             if(nums[lo]<=nums[mid])
             {
-                if(nums[lo]<=target && nums[mid]>=target)
+                if(nums[lo]<=target && nums[mid]>target)
                     hi=mid-1;
                 else
                     lo=mid+1;
             }
             else
             {
-                if(nums[hi]>=target && nums[mid]<=target)
+                if(nums[hi]>=target && nums[mid]<target)
                     lo=mid+1;
                 else
                     hi=mid-1;
