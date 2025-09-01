71caa20c522961ed9d84f03078d5c1096924f94d
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-09-01 11:31:38 +0600

    Time: 0 ms (100.00%) | Memory: 23.6 MB (32.86%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,26 +1,62 @@
+// class MinStack {
+//     stack<int> s1, s2;
+// public:
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         s1.push(val);
+//         if(s2.empty() || val<=s2.top())s2.push(val);
+//     }
+    
+//     void pop() {
+//         if(s1.top()==s2.top())s2.pop();
+//         s1.pop();
+//     }
+    
+//     int top() {
+//         return s1.empty()?-1:s1.top();
+//     }
+    
+//     int getMin() {
+//         return s2.empty()?-1:s2.top();
+//     }
+// };
+
+// Encoded values (O(1) Space)
+typedef long long ll;
 class MinStack {
-    stack<int> s1, s2;
+    stack<ll> s; ll minele;
 public:
     MinStack() {
         
     }
     
     void push(int val) {
-        s1.push(val);
-        if(s2.empty() || val<=s2.top())s2.push(val);
+        if(s.empty()){
+            s.push(val);
+            minele=val;
+        }
+        else if(val<minele){
+            s.push(2LL*val - minele);
+            minele=val;
+        }
+        else s.push(val);
     }
     
     void pop() {
-        if(s1.top()==s2.top())s2.pop();
-        s1.pop();
+        ll t=s.top();s.pop();
+        if(t<minele)
+            minele=2*minele-t;
     }
     
     int top() {
-        return s1.empty()?-1:s1.top();
+        return s.top()<minele?(int)minele:(int)s.top();
     }
     
     int getMin() {
-        return s2.empty()?-1:s2.top();
+        return (int)minele;
     }
 };
 
