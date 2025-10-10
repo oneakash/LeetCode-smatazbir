a6c5fe921e71dae1ee688a78f723354ca800d4c4
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-10-10 21:44:12 +0600

    Time: 2 ms (77.55%) | Memory: 7.6 MB (86.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
+    static constexpr int mx = 31;
+    int found[mx]={0};
+    int dp[mx];
+    int rec(int n){
+        if(found[n]==1)return dp[n];
+        if(n<=1)return n;
+        found[n]=1;
+        return dp[n]=rec(n-1)+rec(n-2);
+    }
     int fib(int n) {
-        if(n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);
+        return rec(n);
     }
 };
