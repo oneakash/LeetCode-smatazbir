1ada2e878779a666cabe60a56cbd160c60bee251
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-12-18 18:19:23 +0600

    Time: 12 ms (28.79%) | Memory: 13.5 MB (67.58%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -1,32 +1,55 @@
+// class Solution {
+// public:
+//     int calculate(string s) {
+//         stack<int> st;
+//         char sgn = '+';
+//         long n = 0;
+//         for(int i = 0; i<s.size(); i++){
+//             if(isdigit(s[i])){
+//                 n = n*10 + (s[i]-'0');
+//             }
+//             if((!isdigit(s[i]) && !isspace(s[i])) or i == s.size()-1){
+//                 if(sgn=='-') st.push(-n);
+//                 else if(sgn=='+')st.push(n);
+//                 else{
+//                     if(sgn=='*'){
+//                         n = st.top()*n;
+//                     }else{
+//                         n = st.top()/n;
+//                     }
+//                     st.pop();st.push(n);
+//                 }
+//                 sgn = s[i];
+//                 n=0;
+//             }
+//         }
+//         while(st.size()){
+//             n += st.top();
+//             st.pop();
+//         }
+//         return n;
+//     }
+// };
+
+// In O(1) Memory - without stack
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> st;
-        char sgn = '+';
-        long n = 0;
-        for(int i = 0; i<s.size(); i++){
-            if(isdigit(s[i])){
-                n = n*10 + (s[i]-'0');
+        istringstream expr_stream('+'+s+'+');
+        long total = 0, term = 0, n;
+        char op;
+        while(expr_stream >> op){
+            if(op=='+' || op=='-'){
+                total += term;
+                expr_stream >> term;
+                term *= 44 - op; // ASCII 43->'+' and 45->'-' => 44 - '+'=1, 44 - '-' = -1
             }
-            if((!isdigit(s[i]) && !isspace(s[i])) or i == s.size()-1){
-                if(sgn=='-') st.push(-n);
-                else if(sgn=='+')st.push(n);
-                else{
-                    if(sgn=='*'){
-                        n = st.top()*n;
-                    }else{
-                        n = st.top()/n;
-                    }
-                    st.pop();st.push(n);
-                }
-                sgn = s[i];
-                n=0;
+            else{
+                expr_stream >> n;
+                if(op=='*')term *= n;
+                else term /= n;
             }
         }
-        while(st.size()){
-            n += st.top();
-            st.pop();
-        }
-        return n;
+        return total;
     }
 };
