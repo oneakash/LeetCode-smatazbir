47851e18a6046790e749d603f7cd6bc7b6bec1c0
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-12-20 08:35:58 +0600

    Time: 0 ms (100.00%) | Memory: 8.3 MB (94.73%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     string makeGood(string s) {
-        stack<char> st;
-        for(char c:s){
-            if(!st.empty() && abs(c-st.top())==32){
-                st.pop();
-            }else{
-                st.push(c);
-            }
-        }
         string ans;
-        while(!st.empty()){
-            ans = st.top()+ans;
-            st.pop();
+        for(char c:s){
+            if(!ans.empty() && abs(c-ans.back())==32){
+                ans.pop_back();
+            }else{
+                ans.push_back(c);
+            }
         }
         return ans;
     }
