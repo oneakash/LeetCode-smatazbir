2020fac40b1efc1d85973186c4c8a803f8bfcb0b
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2025-12-30 07:42:22 +0600

    Time: 0 ms (100.00%) | Memory: 11.8 MB (35.38%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int n = students.size();
-        stack<int>sand;
-        queue<int>stu;
-        int j = n;
-        for(int i = 0; i<n; i++){
-            sand.push(sandwiches[--j]);
-            stu.push(students[i]);
+        // int n = students.size();
+        // stack<int>sand;
+        // queue<int>stu;
+        // int j = n;
+        // for(int i = 0; i<n; i++){
+        //     sand.push(sandwiches[--j]);
+        //     stu.push(students[i]);
+        // }
+        // int lastserved = 0;
+        // while(!stu.empty() && lastserved<stu.size()){
+        //     if(sand.top()!=stu.front()){
+        //         lastserved++;
+        //         stu.push(stu.front());
+        //         stu.pop();
+        //     }else{
+        //         lastserved=0;
+        //         sand.pop();
+        //         stu.pop();
+        //     }
+        // }
+        // return stu.size();
+
+
+        int csc = 0, ssc = 0;
+        for(int i = 0; i<students.size(); i++){
+            if(!students[i])csc++;
+            else ssc++;
         }
-        int lastserved = 0;
-        while(!stu.empty() && lastserved<stu.size()){
-            if(sand.top()!=stu.front()){
-                lastserved++;
-                stu.push(stu.front());
-                stu.pop();
-            }else{
-                lastserved=0;
-                sand.pop();
-                stu.pop();
-            }
+        for(int i = 0; i<sandwiches.size(); i++){
+            if(sandwiches[i] && !ssc)
+                return csc;
+            if(!sandwiches[i] && !csc)
+                return ssc;
+            if(sandwiches[i])ssc--;
+            else csc--;
         }
-        return stu.size();
+        return 0;
+        //O(1) space
     }
 };
