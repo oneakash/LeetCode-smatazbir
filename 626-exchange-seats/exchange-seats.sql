964cd92fae6bc991298e97064741d2b0b9595fc6
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2026-01-29 01:18:18 +0600

    Time: 315 ms (80.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select 
+case when id%2 and id+1 in (select id from seat) then id+1
+when id%2=0 then id-1
+else id end as id,
+student
+from seat
+order by id;
