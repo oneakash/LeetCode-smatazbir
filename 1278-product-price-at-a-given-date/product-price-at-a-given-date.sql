976fe31991f31532a7fd6e4fca00c923ff3629ab
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2026-01-28 20:25:18 +0600

    Time: 522 ms (38.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+with latest as (select
+product_id, max(change_date) as latest_date
+from products
+where change_date<="2019-08-16"
+group by product_id)
+select latest.product_id, new_price as price
+from latest join products on products.product_id=latest.product_id
+and products.change_date=latest.latest_date
+union
+select distinct product_id, 10 as price
+from products
+where product_id not in (
+    select product_id
+    from products
+    where change_date<="2019-08-16"
+);
