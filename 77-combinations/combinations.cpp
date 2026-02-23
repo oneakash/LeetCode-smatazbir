cba15c395ebc72b5fd70c796f1c97ceaf72444a3
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2026-02-23 12:52:15 +0600

    Time: 39 ms (66.18%) | Memory: 63.7 MB (56.45%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,21 +1,21 @@
+//lambda
 class Solution {
-private:
-    void backtrack(int start, int n, int k, vector<int> &curr, vector<vector<int>> &res){
-        if(curr.size() == k){
-            res.push_back(curr);
-            return;
-        }
-        for(int i = start; i<= n-(k-curr.size())+1; i++){
-            curr.push_back(i);
-            backtrack(i+1, n, k, curr, res);
-            curr.pop_back();
-        }
-    }
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> res;
         vector<int> curr;
-        backtrack(1, n, k, curr, res);
+        function<void(int)> backtrack = [&](int start){
+            if(curr.size()==k){
+                res.push_back(curr);
+                return;
+            }
+            for(int i = start; i<=n-(k-curr.size())+1; i++){
+                curr.push_back(i);
+                backtrack(i+1);
+                curr.pop_back();
+            }
+        };
+        backtrack(1);
         return res;
     }
 };
