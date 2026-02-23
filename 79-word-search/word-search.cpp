8e42762bbfd14ef98bc4332d405624b2a3c4f3a1
Md. Tazbir Hossain Akash <107130727+oneakash@users.noreply.github.com>
2026-02-23 14:24:31 +0600

    Time: 252 ms (57.65%) | Memory: 10.3 MB (93.60%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size(), n = board[0].size();
-        function<bool(int, int, int)> dfs = [&](int i, int j, int index){
+        auto dfs = [&](auto&& self, int i, int j, int index)-> bool{
             if(index==word.length())
                 return true;
             if(i<0 || i>=m || j<0 || j>=n || board[i][j]!=word[index])
                 return false;
             char curr = board[i][j];
             board[i][j]='#';
-            bool found = dfs(i, j+1, index+1) || dfs(i, j-1, index+1)
-                    || dfs(i+1, j, index+1) || dfs(i-1, j, index+1);
+            bool found = self(self, i, j+1, index+1) || self(self, i, j-1, index+1)
+                    || self(self, i+1, j, index+1) || self(self, i-1, j, index+1);
             board[i][j] = curr;
             return found;
         };
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if(dfs(i, j, 0))
+                if(dfs(dfs, i, j, 0))
                     return true;
             }
         }
